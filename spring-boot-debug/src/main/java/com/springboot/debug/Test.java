9d1b0222915c6fade48909c50cbbd058709b3e97
wz-dazhi <742820195@qq.com>
2022-04-01 14:37:08 +0800

    加入测试项目, spring为本地编译之后并加入注释的版本

---
+++ spring-boot-debug/src/main/java/com/springboot/debug/Test.java
@@ -0,0 +1,22 @@
+package com.springboot.debug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName: spring-boot-build
+ * @package: com.springboot.debug
+ * @className: Test
+ * @description:
+ * @author: zhi
+ * @date: 2021/7/15
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class Test {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test.class, args);
+	}
+
+}
